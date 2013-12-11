bee2259673c16cfacea6931d1a5e43a3200da72a
Dennis Rassmann <showp1984@gmail.com>
2013-12-11 02:25:09 +0100

    expose build details to public if project is public, hide buttons if not logged in
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -1,9 +1,12 @@
 class BuildsController < ApplicationController
-  before_filter :authenticate_user!, except: [:status]
+  before_filter :authenticate_user!, except: [:status, :show]
   before_filter :project
-  before_filter :authorize_access_project!, except: [:status]
+  before_filter :authorize_access_project!, except: [:status, :show]
 
   def show
+    unless @project.public || current_user
+      authenticate_user! and return
+    end
     @builds = builds
 
     @build = if params[:bid]
